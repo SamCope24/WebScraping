42eeff3b907ecf9a0f904746a21ac3a675abbaf0
Sam <samcope24@gmail.com>
2022-11-28 22:48:25 +0000

    start scraping

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Let's get scraping!");
+
+var pageContent = await CallUrl("https://en.wikipedia.org/wiki/List_of_programmers");
+Console.WriteLine(pageContent);
+
+static async Task<string> CallUrl(string url)
+{
+    HttpClient httpClient = new();
+    var response = await httpClient.GetStringAsync(url);
+    return response;
+}
