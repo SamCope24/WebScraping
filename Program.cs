01793bf1b19910eca538f00a1056d3e2857a2cca
Sam <samcope24@gmail.com>
2022-11-28 23:04:42 +0000

    parse the scraped html

--- Program.cs
+++ Program.cs
@@ -1,11 +1,39 @@
-﻿Console.WriteLine("Let's get scraping!");
+﻿using HtmlAgilityPack;
+
+Console.WriteLine("Let's get scraping!");
 
 var pageContent = await CallUrl("https://en.wikipedia.org/wiki/List_of_programmers");
-Console.WriteLine(pageContent);
+var parsedHtml = ParseHtml(pageContent);
 
-static async Task<string> CallUrl(string url)
+foreach (var item in parsedHtml)
+{
+    Console.WriteLine(item);
+}
+
+static Task<string> CallUrl(string url)
 {
     HttpClient httpClient = new();
-    var response = await httpClient.GetStringAsync(url);
-    return response;
+    return httpClient.GetStringAsync(url);
+}
+
+static List<string> ParseHtml(string html)
+{
+    HtmlDocument htmlDoc = new();
+    htmlDoc.LoadHtml(html);
+
+    var programmerLinkNodes = htmlDoc.DocumentNode.Descendants("li")
+        .Where(node => !node.GetAttributeValue("class", "").Contains("tocsection"))
+        .ToList();
+
+    List<string> wikiLinks = new();
+
+    foreach (var node in programmerLinkNodes)
+    {
+        if (node.FirstChild.Attributes.Count > 0)
+        {
+            wikiLinks.Add(node.FirstChild.Attributes[0].Value);
+        }
+    }
+
+    return wikiLinks;
 }
